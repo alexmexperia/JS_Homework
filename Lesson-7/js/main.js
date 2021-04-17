1e43ef2ca21dd23de37c273d82e3235fffd459d2
Aliaksei <am@whitemedia.by>
2021-04-17 12:34:32 +0300

    Homework 7

---
+++ Lesson-7/js/main.js
@@ -0,0 +1,9 @@
+function showMenu(){
+    document.getElementsByClassName('navigation_mobile')[0].style.left='0px';
+    document.getElementsByClassName('navigation_mobile')[0].style.transition='.5s';
+}
+function hideMenu(){
+    document.getElementsByClassName('navigation_mobile')[0].style.left='-400px';
+    document.getElementsByClassName('navigation_mobile')[0].style.transition='.5s';
+}
+
